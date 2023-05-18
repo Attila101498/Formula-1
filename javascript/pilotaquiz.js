e55e198ad1bd22411c885c8acd6d0b813a343f70
Attila101498 <attila101498@gmail.com>
2023-05-18 23:52:54 +0200

    Drivers, Quiz

--- javascript/pilotaquiz.js
+++ javascript/pilotaquiz.js
@@ -1,40 +1,163 @@
-function submitAnswers() {
-    var total = 10;
-    var score = 0;
+var myQuestions = [
+    {
+        question: "1. Kik alkotják a Ferrari pilótáit a 2019-es idényben?",
+        answers: {
+            a: 'Sebastian Vettel & Daniel Ricciardo',
+            b: 'Sebastian Vettel & Charles Leclerc',
+            c: 'Kimi Räikkönen & Alexander Albon',
+            d: 'Kimi Räikkönen & Charles Leclerc'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "2. Melyik Kevin Magnussen rajtszáma?",
+        answers: {
+            a: '7',
+            b: '12',
+            c: '15',
+            d: '20'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "3. Hogy hívják a McLaren újoncát?",
+        answers: {
+            a: 'Lance Stroll',
+            b: 'Pierre Gasly',
+            c: 'Lando Norris',
+            d: 'George Russell'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "4. Ki Lewis Hamilton csapattársa?",
+        answers: {
+            a: 'Valtteri Bottas',
+            b: 'Sebastian Vettel',
+            c: 'Max Verstappen',
+            d: 'Robert Kubica'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "5. Igaz-e az alábbi állítás? Sergio Péreznek van világbajnoki címe.",
+        answers: {
+            a: 'Igaz',
+            b: 'Hamis'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "6. Kik alkotják a Red Bull Racing pilótáit a 2019-es idényben?",
+        answers: {
+            a: 'Pierre Gasly & Max Verstappen',
+            b: 'Sebastian Vettel & Pierre Gasly',
+            c: 'Max Verstappen & Nico Hülkenberg',
+            d: 'Antonio Giovinazzi & Max Verstappen'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "7. Melyik Daniel Ricciardo rajtszáma?",
+        answers: {
+            a: '1',
+            b: '3',
+            c: '16',
+            d: '77'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "8. Hány világbajnoki címmel rendelkezik Kimi Räikkönen?",
+        answers: {
+            a: '1',
+            b: '2',
+            c: '3',
+            d: '4'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "9. Ki Danyiil Kvjat csapattársa?",
+        answers: {
+            a: 'Antonio Giovinazzi',
+            b: 'Lando Norris',
+            c: 'Alexander Albon',
+            d: 'Carlos Sainz Jr.'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "10. Milyen nemzetiségű Robert Kubica?",
+        answers: {
+            a: 'brit',
+            b: 'finn',
+            c: 'ausztrál',
+            d: 'lengyel'
+        },
+        correctAnswer: 'd'
+    }
+];
 
-    var q1 = document.forms['quizForm']['q1'].value;
-    var q2 = document.forms['quizForm']['q2'].value;
-    var q3 = document.forms['quizForm']['q3'].value;
-    var q4 = document.forms['quizForm']['q4'].value;
-    var q5 = document.forms['quizForm']['q5'].value;
-    var q6 = document.forms['quizForm']['q6'].value;
-    var q7 = document.forms['quizForm']['q7'].value;
-    var q8 = document.forms['quizForm']['q8'].value;
-    var q9 = document.forms['quizForm']['q9'].value;
-    var q10 = document.forms['quizForm']['q10'].value;
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 
-    for (var i = 1; i <= total; i++) {
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
-        //Az üresen hagyott mezőkre egy alert box ugrik fel
-        if (eval('q' + i) == null || eval('q' + i) == '') {
-            alert('Kihagyta a(z) ' + i + ". kérdést!");
-            return false;
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+
+    function showQuestions(questions, quizContainer) {
+        let output = [];
+        let answers;
+
+        for(let i = 0; i < questions.length; i++) {
+
+            answers = [];
+
+            for (letter in questions[i].answers) {
+
+                answers.push(
+                    '<label>'
+                    + '<input type="radio" name="question' + i + '" value="' + letter + '">'
+                    + letter + '. '
+                    + questions[i].answers[letter]
+                    + '</label><br>'
+                );
+            }
+
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
         }
+
+        quizContainer.innerHTML = output.join('');
     }
 
-    var answers = ['b', 'd', 'c', 'a', 'b', 'a', 'b', 'a', 'c', 'd'];
+    function showResults(questions, quizContainer, resultsContainer) {
+        let answerContainers = quizContainer.querySelectorAll('.answers');
+        let userAnswer = '';
+        let numCorrect = 0;
 
-    for (i = 1; i <= total; i++) {
+        for(let i = 0; i < questions.length; i++) {
+            userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
 
-        if (eval('q' + i) == answers[i - 1]) {
-            score++;
+            if (userAnswer === questions[i].correctAnswer) {
+                numCorrect++;
+                answerContainers[i].style.color = 'green';
+            } else {
+                answerContainers[i].style.color = 'red';
+            }
         }
 
+        resultsContainer.innerHTML = 'Eredményed: ' + '<b>' + numCorrect + '</b>' + ' / ' + '<b>' + questions.length + '</b>';
+        alert('Eredményed: ' + numCorrect + ' / ' + questions.length);
     }
 
-    var results = document.getElementById('results');
+    showQuestions(questions, quizContainer);
 
-    results.innerHTML = '<h3> Eredményed: <strong>' + score + '</strong> / <strong>' + total + '</strong> </h3>';
-    alert('Eredményed: ' + score + ' / ' + total);
-    return false;
+    submitButton.onclick = function () {
+        showResults(questions, quizContainer, resultsContainer);
+    }
 }
--- konstruktor.js
+++ konstruktor.js
@@ -10,95 +10,95 @@ var hely9 = 5;
 var hely10 = 5;
 
 
-//Gombnyomásra elinduló autók
+// Gombnyomásra elinduló autók
 function osszesIndul() {
     let k1 = document.getElementById("kep1");
-    let aktualPointMer = 257;
+    let aktualPointMer = 739;
     document.getElementById("demo").innerHTML = aktualPointMer + " pont.";
     if (hely1 < aktualPointMer) {
-        hely1 += 0.5;
-        k1.style.left = hely1 + "%";
+        hely1 += 0.3;
+        k1.style.left = hely1 + "px";
         setTimeout("osszesIndul()", 40);
     }
 
     let k2 = document.getElementById("kep2");
-    let aktualPointRedB = 110;
+    let aktualPointRedB = 417;
     document.getElementById("demo2").innerHTML = aktualPointRedB + " pont.";
     if (hely2 < aktualPointRedB) {
-        hely2 += 0.5;
-        k2.style.left = hely2 + "%";
+        hely2 += 0.3;
+        k2.style.left = hely2 + "px";
         setTimeout("osszesIndul()", 40);
     }
 
     let k3 = document.getElementById("kep3");
-    let aktualPointFer = 139;
+    let aktualPointFer = 504;
     document.getElementById("demo3").innerHTML = aktualPointFer + " pont.";
     if (hely3 < aktualPointFer) {
-        hely3 += 0.5;
-        k3.style.left = hely3 + "%";
+        hely3 += 0.3;
+        k3.style.left = hely3 + "px";
         setTimeout("osszesIndul()", 40);
     }
 
     let k4 = document.getElementById("kep4");
-    let aktualPointRp = 17;
+    let aktualPointRp = 73;
     document.getElementById("demo4").innerHTML = aktualPointRp + " pont.";
     if (hely4 < aktualPointRp) {
-        hely4 += 0.5;
-        k4.style.left = hely4 + "%";
+        hely4 += 0.3;
+        k4.style.left = hely4 + "px";
         setTimeout("osszesIndul()", 40);
     }
 
     let k5 = document.getElementById("kep5");
-    let aktualPointWill = 0;
+    let aktualPointWill = 1;
     document.getElementById("demo5").innerHTML = aktualPointWill + " pont.";
     if (hely5 < aktualPointWill) {
-        hely5 += 0.5;
-        k5.style.left = hely5 + "%";
+        hely5 += 0.3;
+        k5.style.left = hely5 + "px";
         setTimeout("osszesIndul()", 40);
     }
 
     let k6 = document.getElementById("kep6");
-    let aktualPointMcl = 30;
+    let aktualPointMcl = 145;
     document.getElementById("demo6").innerHTML = aktualPointMcl + " pont.";
     if (hely6 < aktualPointMcl) {
-        hely6 += 0.5;
-        k6.style.left = hely6 + "%";
+        hely6 += 0.3;
+        k6.style.left = hely6 + "px";
         setTimeout("osszesIndul()", 40);
     }
 
     let k7 = document.getElementById("kep7");
-    let aktualPointTr = 16;
+    let aktualPointTr = 85;
     document.getElementById("demo7").innerHTML = aktualPointTr + " pont.";
     if (hely7 < aktualPointTr) {
-        hely7 += 0.5;
-        k7.style.left = hely7 + "%";
+        hely7 += 0.3;
+        k7.style.left = hely7 + "px";
         setTimeout("osszesIndul()", 40);
     }
 
     let k8 = document.getElementById("kep8");
-    let aktualPointHaas = 16;
+    let aktualPointHaas = 28;
     document.getElementById("demo8").innerHTML = aktualPointHaas + " pont.";
     if (hely8 < aktualPointHaas) {
-        hely8 += 0.5;
-        k8.style.left = hely8 + "%";
+        hely8 += 0.3;
+        k8.style.left = hely8 + "px";
         setTimeout("osszesIndul()", 40);
     }
 
     let k9 = document.getElementById("kep9");
-    let aktualPointRen = 14;
+    let aktualPointRen = 91;
     document.getElementById("demo9").innerHTML = aktualPointRen + " pont.";
     if (hely9 < aktualPointRen) {
-        hely9 += 0.5;
-        k9.style.left = hely9 + "%";
+        hely9 += 0.3;
+        k9.style.left = hely9 + "px";
         setTimeout("osszesIndul()", 40);
     }
 
     let k10 = document.getElementById("kep10");
-    let aktualPointAlfa = 13;
+    let aktualPointAlfa = 57;
     document.getElementById("demo10").innerHTML = aktualPointAlfa + " pont.";
     if (hely10 < aktualPointAlfa) {
-        hely10 += 0.5;
-        k10.style.left = hely10 + "%";
+        hely10 += 0.3;
+        k10.style.left = hely10 + "px";
         setTimeout("osszesIndul()", 40);
     }
 }
